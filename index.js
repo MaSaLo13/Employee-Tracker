9fa8c4ddf4c87fa8a9ba32917d063f797631c7d6
marvinsantiago <marvinsantiago@MindYouBusiness.lan>
2022-08-15 11:53:33 -0700

    had to move init() in both addDept and addEmployee into .then instead of outside of it. Now all prompts are properly showing up

--- index.js
+++ index.js
@@ -14,166 +14,164 @@ const db = mysql.createConnection(
   console.log(`Connected to the employtrack database.`)
 );
 
-const init  = () => {
-        inquirer.prompt([
-            {
-                type: 'list',
-                message: 'What would you like to do?',
-                name: 'choice',
-                choices: ['View All Employees', 'Add Employee', "Update Employee Role", "View All Roles", "View All Departments", "Add Department", "Quit"],
-            },
-        ])
+const init = () => {
+  inquirer.prompt([
+    {
+      type: 'list',
+      message: 'What would you like to do?',
+      name: 'choice',
+      choices: ['View All Employees', 'Add Employee', "Update Employee Role", "View All Roles", "View All Departments", "Add Department", "Quit"],
+    },
+  ])
 
-        .then(answer => {
-            switch (answer.choice) {
-                case 'View All Employees':
-                    viewEmployee();
-                    break;
+    .then(answer => {
+      switch (answer.choice) {
+        case 'View All Employees':
+          viewEmployee();
+          break;
 
-                case 'Add Employee':
-                    addEmployee()
-                    break;
+        case 'Add Employee':
+          addEmployee()
+          break;
 
-                case 'Update Employee Role':
-                    updateRole()
-                    break;
+        case 'Update Employee Role':
+          updateRole()
+          break;
 
-                case 'View All Roles':
-                    viewRoles();
-                    break;
+        case 'View All Roles':
+          viewRoles();
+          break;
 
-                case 'Add Role':
-                    addRole()
-                    break;
-  
-                case 'View All Departments':
-                    viewDept()
-                    break;
-  
-                case 'Add Department':
-                    addDept();
-                    break;
-                
-                case 'Quit':
-                    exit();
-                    break;
-            }
-        });
-    };
+        case 'Add Role':
+          addRole()
+          break;
+
+        case 'View All Departments':
+          viewDept()
+          break;
+
+        case 'Add Department':
+          addDept();
+          break;
+
+        case 'Quit':
+          exit();
+          break;
+      }
+    });
+};
 
 function viewEmployee() {
   db.query("SELECT employee.id as 'Employee ID', employee.first_name AS 'First Name', employee.last_name AS 'Last Name', department.name AS Department, roles.salary AS Salary, CONCAT(manager.first_name,' ',manager.last_name )  AS Manager, roles.title AS Role FROM employee LEFT JOIN roles ON employee.role_id = roles.id LEFT JOIN department ON roles.department_id = department.id LEFT JOIN employee manager ON manager.id = employee.manager_id ORDER BY employee.id", function (err, data) {
     console.log(data);
     if (err) throw err;
-          init();
-      });
-  };
+    init();
+  });
+};
 
 function addEmployee() {
-    inquirer.prompt([
-      {
-        type: 'input',
-        name: 'first_name',
-        message: "What is the employee's first name?"
-      },
-      {
-        type: 'input',
-        name: 'last_name',
-        message: "what is the employee's last name?"
-      },
-      // {
-      //   type: 'list',
-      //   name: 'Role',
-      //   message: 'What is the employees role?',
-      //   choices: ["Sales Lead", "Sales Person", "Lead Engineer", "Software Engineer", "Account Manager", "Accountant", "Legal Team Lead", "Lawyer"],
-      // },
-      {
-        type: 'list',
-        name: 'role_id',
-        message: 'What is the employees role_id?',
-        choices: ["1", "2", "3", "4", "5", "6", "7", "8"],
-      },
-      // {
-      //   type: 'list',
-      //   name: 'Manager',
-      //   message: 'What is the employees manager?',
-      //   choices: ["None", "John Doe", "Ashley Rodriguez", "Kunal Singh", "Sarah Lourd"],
-      // },
-    ])
-    .then(function(answer) {
-			//*Need to add role name and then find length of role array to add ID #
-			let fN = answer.first_name;
-			let lN = answer.last_name;
-			let role_id = answer.role_id;
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'first_name',
+      message: "What is the employee's first name?"
+    },
+    {
+      type: 'input',
+      name: 'last_name',
+      message: "what is the employee's last name?"
+    },
+    // {
+    //   type: 'list',
+    //   name: 'Role',
+    //   message: 'What is the employees role?',
+    //   choices: ["Sales Lead", "Sales Person", "Lead Engineer", "Software Engineer", "Account Manager", "Accountant", "Legal Team Lead", "Lawyer"],
+    // },
+    {
+      type: 'list',
+      name: 'role_id',
+      message: 'What is the employees role_id?',
+      choices: ["1", "2", "3", "4", "5", "6", "7", "8"],
+    },
+    // {
+    //   type: 'list',
+    //   name: 'Manager',
+    //   message: 'What is the employees manager?',
+    //   choices: ["None", "John Doe", "Ashley Rodriguez", "Kunal Singh", "Sarah Lourd"],
+    // },
+  ])
+    .then(function (answer) {
+      //*Need to add role name and then find length of role array to add ID #
+      let fN = answer.first_name;
+      let lN = answer.last_name;
+      let role_id = answer.role_id;
       // let manager = answer.Manager;
 
-			let addNewEmployee = {first_name:fN, last_name:lN, role_id:role_id};
-			db.query('INSERT INTO employee SET ?', addNewEmployee)
+      let addNewEmployee = { first_name: fN, last_name: lN, role_id: role_id }
+      db.query('INSERT INTO employee SET ?', addNewEmployee)
+      init();
     })
-    init()
-    
-  }; 
-
-  // function addDept() {
-  //   inquirer.prompt([
-  //     {
-  //       type: 'input',
-  //       name: 'department',
-  //       message: 'What is the name of the department?'
-  //     }
-  //   ])
-  //   .then(function(answer) {
-  //     let department = {name:answer.department};
-  //     db.query ("INSERT INTO department SET ?;", department)
-  //   });
-  //   init()
-  //  };
+};
+// function addDept() {
+//   inquirer.prompt([
+//     {
+//       type: 'input',
+//       name: 'department',
+//       message: 'What is the name of the department?'
+//     }
+//   ])
+//   .then(function(answer) {
+//     let department = {name:answer.department};
+//     db.query ("INSERT INTO department SET ?;", department)
+//   });
+//   init()
+//  };
 
 function updateRole() {
   db.query("SELECT * FROM employee", function (err, data) {
     console.log(data);
     if (err) throw err;
-           init();
-          });
-  };
+    init();
+  });
+};
 
 function viewRoles() {
   db.query("SELECT roles.id as ID, roles.title as title, department.name as Department, roles.salary as Salary  FROM roles LEFT JOIN department ON roles.department_id = department.id;", function (err, data) {
     console.log(data);
     if (err) throw err;
-          init();
-          });
-  }; 
+    init();
+  });
+};
 
-  function addRole() {
-    inquirer.prompt([
-      {
-        type: 'input',
-        name: 'roleName',
-        message: "What is the name of the role?"
-      },
-      {
-        type: 'input',
-        name: 'salary',
-        message: "What is the salary of the role?"
-      },
-      {
-        type: 'list',
-        name: 'Department',
-        message: 'Which department does the role belong to?',
-        choices: ["Engineering", "Finance", "Legal", "Sales"],
-      },
-    
-    ])
-  }
+function addRole() {
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'roleName',
+      message: "What is the name of the role?"
+    },
+    {
+      type: 'input',
+      name: 'salary',
+      message: "What is the salary of the role?"
+    },
+    {
+      type: 'list',
+      name: 'Department',
+      message: 'Which department does the role belong to?',
+      choices: ["Engineering", "Finance", "Legal", "Sales"],
+    },
+
+  ])
+}
 
 function viewDept() {
   db.query("SELECT * FROM department", function (err, data) {
     console.log(data);
     if (err) throw err;
-          init();
-          });
-  }; 
+    init();
+  });
+};
 
 function addDept() {
   inquirer.prompt([
@@ -183,12 +181,12 @@ function addDept() {
       message: 'What is the name of the department?'
     }
   ])
-  .then(function(answer) {
-    let department = {name:answer.department};
-    db.query ("INSERT INTO department SET ?;", department)
-  });
-  init()
- };
+    .then(function (answer) {
+      let department = { name: answer.department };
+      db.query("INSERT INTO department SET ?;", department)
+      init()
+    });
+};
 
 
 init();
